32956f28b1506be05c0e50148a069b4e24acf04d
Lucas Guimarães <89198796+LucasRGuimaraes@users.noreply.github.com>
2022-09-28 15:52:43 -0300

    Separação da lista de times para limpar o codigo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,7 @@ import { TabContext, TabList, TabPanel } from "@mui/lab";
 import { useState } from "react";
 import { FiMenu, FiShield } from "react-icons/fi";
 import { LogoGe } from "./LogoGe";
+import { serieA, serieB, europa } from "./utils/TimesList";
 
 export function Header() {
   const [settingsMenu, setSettingsMenu] = useState(null);
@@ -93,6 +94,7 @@ export function Header() {
                   sx={{
                     display: "grid",
                     gridTemplateColumns: "repeat(4, 1fr)",
+                    padding: "10px",
                   }}
                 >
                   {serieA.map((item) => (
@@ -139,106 +141,3 @@ export function Header() {
     </AppBar>
   );
 }
-
-const serieA = [
-  {
-    name: "América-MG",
-    img: "https://s.sde.globo.com/media/organizations/2019/02/28/America-MG-VERDE-fev2019-01.svg",
-    link: "https://ge.globo.com/futebol/times/america-mg/",
-  },
-  {
-    name: "Athletico-PR",
-    img: "https://s.sde.globo.com/media/organizations/2019/09/09/Athletico-PR.svg",
-    link: "https://ge.globo.com/pr/futebol/times/athletico-pr/",
-  },
-  {
-    name: "Atlético-GO",
-    img: "https://s.sde.globo.com/media/organizations/2020/07/02/atletico-go-2020.svg",
-    link: "https://ge.globo.com/go/futebol/times/atletico-go/",
-  },
-  {
-    name: "Atlético-MG",
-    img: "https://s.sde.globo.com/media/organizations/2018/03/10/atletico-mg.svg",
-    link: "https://ge.globo.com/futebol/times/atletico-mg/",
-  },
-  {
-    name: "Avaí",
-    img: "https://s.sde.globo.com/media/organizations/2018/09/04/avai-futebol-clube.svg",
-    link: "https://ge.globo.com/sc/futebol/times/avai/",
-  },
-  {
-    name: "Botafogo",
-    img: "https://s.sde.globo.com/media/organizations/2019/02/04/botafogo-svg.svg",
-    link: "https://ge.globo.com/futebol/times/botafogo/",
-  },
-  {
-    name: "Bragantino",
-    img: "https://s.sde.globo.com/media/organizations/2021/06/28/bragantino.svg",
-    link: "https://ge.globo.com/sp/vale-do-paraiba-regiao/futebol/times/bragantino/",
-  },
-  {
-    name: "Ceará",
-    img: "	https://s.sde.globo.com/media/organizations/2019/10/10/ceara.svg",
-    link: "https://ge.globo.com/ce/futebol/times/ceara/",
-  },
-  {
-    name: "Corinthians",
-    img: "https://s.sde.globo.com/media/organizations/2019/09/30/Corinthians.svg",
-    link: "https://ge.globo.com/futebol/times/corinthians/",
-  },
-  {
-    name: "Coritiba",
-    img: "https://s.sde.globo.com/media/organizations/2018/03/11/coritiba.svg",
-    link: "https://ge.globo.com/pr/futebol/times/coritiba/",
-  },
-  {
-    name: "Cuiabá",
-    img: "https://s.sde.globo.com/media/organizations/2018/12/26/Cuiaba_EC.svg",
-    link: "https://ge.globo.com/mt/futebol/times/cuiaba/",
-  },
-  {
-    name: "Flamengo",
-    img: "https://s.sde.globo.com/media/organizations/2018/04/10/Flamengo-2018.svg",
-    link: "https://ge.globo.com/futebol/times/flamengo/",
-  },
-  {
-    name: "Fluminense",
-    img: "https://s.sde.globo.com/media/organizations/2018/03/11/fluminense.svg",
-    link: "https://ge.globo.com/futebol/times/fluminense/",
-  },
-  {
-    name: "Fortaleza",
-    img: "https://s.sde.globo.com/media/organizations/2021/09/19/Fortaleza_2021_1.svg",
-    link: "https://ge.globo.com/ce/futebol/times/fortaleza/",
-  },
-  {
-    name: "Goiás",
-    img: "	https://s.sde.globo.com/media/organizations/2021/03/01/GOIAS-2021.svg",
-    link: "https://ge.globo.com/go/futebol/times/goias/",
-  },
-  {
-    name: "Internacional",
-    img: "https://s.sde.globo.com/media/organizations/2018/03/11/internacional.svg",
-    link: "https://ge.globo.com/rs/futebol/times/internacional/",
-  },
-  {
-    name: "Juventude",
-    img: "https://s.sde.globo.com/media/organizations/2021/04/29/Juventude-2021-01.svg",
-    link: "https://ge.globo.com/rs/futebol/times/juventude/",
-  },
-  {
-    name: "Palmeiras",
-    img: "https://s.sde.globo.com/media/organizations/2019/07/06/Palmeiras.svg",
-    link: "https://ge.globo.com/futebol/times/palmeiras/",
-  },
-  {
-    name: "Santos",
-    img: "https://s.sde.globo.com/media/organizations/2018/03/12/santos.svg",
-    link: "https://ge.globo.com/sp/santos-e-regiao/futebol/times/santos/",
-  },
-  {
-    name: "São Paulo",
-    img: "https://s.sde.globo.com/media/organizations/2018/03/11/sao-paulo.svg",
-    link: "https://ge.globo.com/futebol/times/sao-paulo/",
-  },
-];
