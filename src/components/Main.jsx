0ea48f53af53e57b7a29924e8037f0cee359db93
Lucas Guimarães <89198796+LucasRGuimaraes@users.noreply.github.com>
2022-09-28 14:12:30 -0300

    Estilização da table

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,21 @@
 import {
     Box,
-    TableHead,
-    TableRow,
-    Paper,
-    TableContainer,
     Table,
     TableBody,
     TableCell,
-    tableCellClasses,
+    TableHead,
+    TableRow,
+    Typography,
 } from "@mui/material";
-import { styled } from "@mui/system";
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
 
 export function Main() {
+    let colocacao = 1;
     const [times, setTimes] = useState([]);
 
     useEffect(() => {
-        api.get("times")
+        api.get("times?_sort=pontos,vitorias&_order=desc,desc")
             .then((response) => {
                 setTimes(response.data);
             })
@@ -26,76 +24,69 @@ export function Main() {
             });
     }, []);
 
-    const StyledTableCell = styled(TableCell)(({ theme }) => ({
-        [`&.${tableCellClasses.head}`]: {
-            backgroundColor: theme.palette.common.black,
-            color: theme.palette.common.white,
-        },
-        [`&.${tableCellClasses.body}`]: {
-            fontSize: 14,
-        },
-    }));
-
-    const StyledTableRow = styled(TableRow)(({ theme }) => ({
-        "&:nth-of-type(odd)": {
-            backgroundColor: theme.palette.action.hover,
-        },
-        // hide last border
-        "&:last-child td, &:last-child th": {
-            border: 0,
-        },
-    }));
+    function getColor(colocacao) {
+        if (colocacao <= 4) {
+            return "#0000FF";
+        } else if (colocacao <= 6) {
+            return "#00FFFF";
+        } else if (colocacao <= 12) {
+            return "#008000";
+        } else if (colocacao <= 16) {
+            return "#999999";
+        } else {
+            return "#FE2F00";
+        }
+    }
 
     return (
-        <Box sx={{ px: 2, pt: 6, pb: 10 }}>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell>CLASSIFICAÇÃO</StyledTableCell>
-                            <StyledTableCell align="right">
-                                PONTOS
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                JOGOS
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                VITÓRIAS
-                            </StyledTableCell>
-                            <StyledTableCell align="right">
-                                EMPATES
-                            </StyledTableCell>
-                            <StyledTableCell align="center">
-                                DERROTAS
-                            </StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {times.map((time) => (
-                            <StyledTableRow key={time.nome}>
-                                <StyledTableCell component="th" scope="row">
-                                    {time.classificacao}. {time.nome}
-                                </StyledTableCell>
-                                <StyledTableCell align="right">
-                                    {time.pontos}
-                                </StyledTableCell>
-                                <StyledTableCell align="right">
+        <Box sx={{ px: 2, paddingBlock: 15 }}>
+            <Table sx={{ minWidth: 700, borderBlock: 0 }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>CLASSIFICAÇÃO</TableCell>
+                        <TableCell align="center">PONTOS</TableCell>
+                        <TableCell align="center">JOGOS</TableCell>
+                        <TableCell align="center">VITÓRIAS</TableCell>
+                        <TableCell align="center">EMPATES</TableCell>
+                        <TableCell align="center">DERROTAS</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {times.map((time) => {
+                        return (
+                            <TableRow key={time.nome} padding="">
+                                <TableCell sx={{ display: "flex" }}>
+                                    <Typography
+                                        fontSize="20px"
+                                        fontWeight="600"
+                                        color={getColor(colocacao)}
+                                    >
+                                        {colocacao++}&nbsp;
+                                    </Typography>
+                                    <Typography>{time.nome}</Typography>
+                                </TableCell>
+                                <TableCell align="center" bgcolor="#F5F5F5">
+                                    <Typography fontWeight="700">
+                                        {time.pontos}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell align="center">
                                     {time.jogos}
-                                </StyledTableCell>
-                                <StyledTableCell align="right">
+                                </TableCell>
+                                <TableCell align="center" bgcolor="#F5F5F5">
                                     {time.vitorias}
-                                </StyledTableCell>
-                                <StyledTableCell align="right">
+                                </TableCell>
+                                <TableCell align="center">
                                     {time.empates}
-                                </StyledTableCell>
-                                <StyledTableCell align="right">
+                                </TableCell>
+                                <TableCell align="center" bgcolor="#F5F5F5">
                                     {time.derrotas}
-                                </StyledTableCell>
-                            </StyledTableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+                                </TableCell>
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
         </Box>
     );
 }
