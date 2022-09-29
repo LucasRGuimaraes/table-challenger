4acb1b75d018e9bd955ebc7e704023ded7bfe45e
Lucas Guimarães <89198796+LucasRGuimaraes@users.noreply.github.com>
2022-09-29 10:01:08 -0300

    Manutençao

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -24,7 +24,7 @@ export function Main() {
         </Typography>
         {/* <TabelaBrasileirao /> */}
         <TabelaDesafioResolvido />
-        <TabelaDesafio />
+        {/* <TabelaDesafio /> */}
       </Box>
     </Box>
   );
